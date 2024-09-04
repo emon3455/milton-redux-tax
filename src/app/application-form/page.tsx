3e8fc94a833b5ffcbee06035212cffeec1a4551c
emon <emon.mhk69@gmail.com>
2024-09-04 18:51:53 +0600

    push

--- src/app/application-form/page.tsx
+++ src/app/application-form/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
     return (
         <div>
             <div className='bg-gradient-to-r from-[#390481] to-[#fe3976]  px-14 py-24'>
-                <h1 className='text-5xl text-center font-semibold mt-14 text-white'>Let's get your savings started</h1>
+                <h1 className='text-5xl text-center font-semibold mt-14 text-white'>Get your savings started</h1>
             </div>
             <ApplicationForm />
         </div>
